66b8a5b2c67504ac12c69a9f4ed540fc2b4c5bb9
G00341457 <G00341457@gmit.ie>
2018-04-04 17:22:22 +0100

    just started to go through the first video with Ian

--- shunt.go
+++ shunt.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("hello World")
+
+func intoPost(infix string)string{
+
+specials := map[rune]int{'*' : 10, '|' : 9, '.' : 8}	
+
+ postfix, s := []rune{}, {}rune{}
+
+ return string(postfix)
+}
+func main(){
+
+    //ab.c*
+	fmt.Println("infix:   ","a.b.c")
+	fmt.Println("Postfix:   ", intoPost("a.b.c"))
+    //abd|.*
+	fmt.Println("infix:   ","a.(b|d)")
+	fmt.Println("Postfix:   ", intoPost("a.(b|d)"))
+    //abd|.c*
+	fmt.Println("infix:   ","a.(b|d).c")
+	fmt.Println("Postfix:   ", intoPost("a.(b|d).c"))
+    //abb.+.c
+	fmt.Println("infix:   ","a.(b.b)+.c")
+	fmt.Println("Postfix:   ", intoPost("a.(b.b)+.c"))
 }
