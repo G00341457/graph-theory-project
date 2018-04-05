4bb061ac39099d5bf142738077cd3244faf0a08b
G00341457 <G00341457@gmit.ie>
2018-04-05 14:37:18 +0100

    completed the nfa page

--- nfa.go
+++ nfa.go
@@ -1,26 +1,68 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-type state struct{
+type state struct {
 	symbol rune
-	edge1 *state
-	edge2 *state
+	edge1  *state
+	edge2  *state
 }
 
-type nfa struct{
-	initial *states
-	accept *state
+type nfa struct {
+	initial *state
+	accept  *state
 }
 
-func poregtonfa(postfix string){
+func poregtonfa(postfix string) {
 
+	nfaStack := []*nfa{}
 
+	for _, r := range postfix {
+		switch r {
+		case '.':
+			frag2 = nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[len(nfaStack)-1]
+			frag1 = nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[len(nfaStack)-1]
+
+			frag1.accept.edge1 = frag2.initial
+
+			nfaStack = append(nfaStack, &nfa{initial: frag1.initial, accept: frag2.accept})
+		case '|':
+			frag2 = nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[len(nfaStack)-1]
+			frag1 = nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[len(nfaStack)-1]
+
+			initial := state{edge1: frag1.initial, edged2: frag2.initial}
+			accept := state{}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		case '*':
+			frag = nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+			
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		default:
+			accept := state{}
+			initial := state{symbol: r, edge1: &accept}		
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		}
+	}
+
+	return nfaStack[0]
 }
 
-func main(){
-	nfa := poregontonfa("lol")
+func main() {
+	nfa := poregontonfa("ab.c*|")
 	fmt.Println(nfa)
 }
